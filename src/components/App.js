c5c8fff2c7b5f749e35df471e251d632f82332b2
baldo-vela <baldo.vela+github@gmail.com>
2021-07-02 00:16:56 -0500

    commit tree 8a0067d74fd7872e36d6c15505f112531ac6cfc0

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,22 @@ class App extends React.Component {
     }
   }
 
+  fetchPets = () => {
+    let endpoint = '/api/pets';
+
+    if (this.state.filters.type !== 'all') {
+      endpoint += `?type=${this.state.filters.type}`;
+    }
+
+    fetch(endpoint)
+      .then(res => res.json())
+      .then(pets => this.setState({pets: pets }));
+  };
+
+  onChangeType = ({ target: { value } }) => {
+    this.setState({ filters: { ...this.state.filters, type: value } });
+  };
+
   render() {
     return (
       <div className="ui container">
